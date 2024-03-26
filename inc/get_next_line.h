0924d991d7857bde75c5508693e1e806bf8b5239
pepedinho <2spii94@gmail.com>
2024-03-26 19:53:36 +0100

    add struct and ft_strdup

--- inc/get_next_line.h
+++ inc/get_next_line.h
@@ -6,14 +6,23 @@
 /*   By: itahri <itahri@contact.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 15:29:14 by itahri            #+#    #+#             */
-/*   Updated: 2024/03/26 15:31:43 by itahri           ###   ########.fr       */
+/*   Updated: 2024/03/26 19:09:56 by itahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 #include <stdlib.h>
 #include <unistd.h>
+
+# define COUNT count.prec ? count.prec : count.now
+
+
+typedef struct s_line
+{
+	int	prec;
+	int	now;
+}		t_line;
+
 
 #endif
